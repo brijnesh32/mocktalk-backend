9d141950a8e21976281c94044d10d5d4ee937eb3
brijnesh32 <brijnesh31@gmail.com>
2025-07-25 16:45:42 +0530

    Ready for Railway

--- reports/urls.py
+++ reports/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+from django.http import JsonResponse
+
 from .views import start_interview, submit_answers, save_report, get_all_reports
 
+def home(request):
+    return JsonResponse({"message": "MockTalk backend is working ✅"})
+
 urlpatterns = [
+    path('', home),  # This will serve the root path "/"
     path("api/start-interview/", start_interview),
     path("api/submit-answers/", submit_answers),
     path('api/save-report/', save_report),
